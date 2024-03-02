576dccdfc62f5e44e65dda62d50769cdc1e36670
jangsungtai <mrgv109@gmail.com>
2024-03-02 14:52:28 +0900

    app modified

--- webserver/app.py
+++ webserver/app.py
@@ -1,65 +1,106 @@
 import streamlit as st
-from businessLogic import transcribeVideoOrchestrator
+import tempfile
+from pprint import pprint
+import whisper
+from pytube import YouTube
 
 
-def open_buy_me_coffee():
-    st.markdown('<script>document.getElementById("buy-me-coffee-btn").click();</script>',
-                unsafe_allow_html=True)
+def transcribeVideoOrchestrator(youtube_url: str,  model_name: str):
+    #url과 모델 변수를 받아서 동영상,텍스트 변환 함수 호출 
+    video = downloadYoutubeVideo(youtube_url)
+    transcription = transcribe(video, model_name)
+    return transcription
+
+def transcribe(video: dict, model_name="medium"):
+    #STT 메인함수 : 지정된 모델으로 동영상의 오디오를 텍스트로 변환 
+    print("Transcribing...", video['name'])
+    print("Using model:", model_name)
+    model = whisper.load_model(model_name)
+    result = model.transcribe(video['path'], ) #***STT 모델 호출
+    pprint(result)
+    return result["text"]
+
+
+def downloadYoutubeVideo(youtube_url: str) -> dict:
+    print("Processing: " + youtube_url)
+    yt = YouTube(youtube_url)
+    directory = tempfile.gettempdir()
+    file_path = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(directory)
+    video_length = yt.length  # 비디오 길이(초 단위)
+    print(f"VIDEO NAME: {yt.title}")
+    print(f"Download complete: {file_path}")
+    return {"name": yt.title, "thumbnail": yt.thumbnail_url, "path": file_path, "duration": video_length}
+
+
+
+def on_progress(stream, chunk, bytes_remaining):
+    #다운로드 진행현황 표시 콜백 함수"
+    """Callback function"""
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    pct_completed = bytes_downloaded / total_size * 100
+    print(f"Status: {round(pct_completed, 2)} %")
+
+
 
 
 def main():
-    st.title("Video2Text")
- # Embed the Buy Me a Coffee widget using the provided <script> tag
-    buy_me_coffee_script = """
-         <script
-            data-name="BMC-Widget"
-            data-cfasync="false"
-            src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-            data-id="hayerhans"
-            data-description="If you like this, please consider supporting this work"
-            data-message="If you like this, please consider supporting me!"
-            data-color="#BD5FFF"
-            data-position="Right"
-            data-x_margin="18"
-            data-y_margin="18">
-        </script>
-    """
-    st.components.v1.html(buy_me_coffee_script)
-    st.title("Support Me on Buy Me a Coffee")
-    st.write(
-        "If you find this app helpful and would like to support me, you can buy me a coffee!")
-    st.markdown(buy_me_coffee_script, unsafe_allow_html=True)
+    st.title("Teaching2Text")
 
     # User input: YouTube URL
     url = st.text_input("Enter YouTube URL:")
+
 
     # User input: model
     models = ["tiny", "base", "small", "medium", "large"]
     model = st.selectbox("Select Model:", models)
     st.write(
         "If you take a smaller model it is faster but not as accurate, whereas a larger model is slower but more accurate.")
+    
+    # User input: model suffix(mode)
+    models_mode = ["Normal mode", "English mode"]
+    model_mode = st.selectbox("Select model_mode:", models_mode)
+    st.write(
+        "Normal mode is good for any language, English mode is optimized specifically for English .")
+
+    # Result from User input : model + suffix
+    if model_mode == "English mode" and model_mode in ["tiny", "base", "small", "medium"]:
+        model = f"{model_mode}_en"
+
+
+    
     if st.button("Transcribe"):
+        
+        #placeholder 선언
+        video_info_placeholder = st.empty()
+        transcription_placeholder = st.empty()
+
         if url:
+            # 비디오 정보를 먼저 Placeholder에 표시
+            video_info = downloadYoutubeVideo(url)
+            video_info_placeholder.subheader("Video Information:")
+            video_info_placeholder.write("Loading Videos")
+
+            # Transcription 시작 *메인부분*
             transcript = transcribeVideoOrchestrator(url, model)
 
             if transcript:
-                st.subheader("Transcription:")
-                st.write(transcript)
+                word_count = len(transcript.split())
+                # 비디오 정보 Placeholder 업데이트
+                video_info_placeholder.write(f"Video Name: {video_info['name']}") # 비디오 이름 예시
+                # Update video information placeholder
+                video_info_placeholder.write(f"Video Name: {video_info['name']}\nTotal Words: {word_count}\nVideo Length: {video_info['duration']} seconds")
+
+                # Calculate word count from transcript
+
+
+
+                # 전체 텍스트를 다른 Placeholder에 표시
+                transcription_placeholder.subheader("Full Text Transcription:")
+                transcription_placeholder.write(transcript)
             else:
                 st.error("Error occurred while transcribing.")
                 st.write("Please try again.")
-
-    st.markdown('<div style="margin-top: 450px;"</div>',
-                unsafe_allow_html=True)
-
-    st.write(
-        "If you need help or have questions about Video2Text, feel free to reach out to me.")
-
-    st.write("Please enter your message below:")
-    user_message = st.text_area("Your Message:")
-
-    st.markdown(
-        f'<a href="mailto:contact@jhayer.tech?subject=Video2Text-Help&body={user_message}">Send Mail</a>', unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
--- webserver/util.py
+++ webserver/util.py
@@ -4,23 +4,25 @@ from pprint import pprint
 import whisper
 from pytube import YouTube
 
-
 def transcribeVideoOrchestrator(youtube_url: str,  model_name: str):
+    #url과 모델 변수를 받아서 동영상,텍스트 변환 함수 호출 
     video = downloadYoutubeVideo(youtube_url)
     transcription = transcribe(video, model_name)
     return transcription
 
 
 def transcribe(video: dict, model_name="medium"):
+    #STT 메인함수 : 지정된 모델으로 동영상의 오디오를 텍스트로 변환 
     print("Transcribing...", video['name'])
     print("Using model:", model_name)
     model = whisper.load_model(model_name)
-    result = model.transcribe(video['path'], )
+    result = model.transcribe(video['path'], ) #***STT 모델 호출
     pprint(result)
     return result["text"]
 
 
 def downloadYoutubeVideo(youtube_url: str) -> dict:
+    #유튜브 동영상을 다운로드 하고 정보를 dic으로 반환
     print("Processing : " + youtube_url)
     yt = YouTube(youtube_url)
     directory = tempfile.gettempdir()
@@ -32,6 +34,7 @@ def downloadYoutubeVideo(youtube_url: str) -> dict:
 
 
 def on_progress(stream, chunk, bytes_remaining):
+    #다운로드 진행현황 표시 콜백 함수"
     """Callback function"""
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
